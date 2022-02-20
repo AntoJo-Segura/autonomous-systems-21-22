63c156ca9ea7b5d98fb69894669fce27838e8487
AJ <anto296@hotmail.com>
2022-02-20 16:34:54 +0100

    not duplicated clauses

--- lab2/clauses.py
+++ lab2/clauses.py
@@ -17,7 +17,14 @@ def all_filled(max_row = 9, max_column = 9):
 assert(all_filled()[0] == [1,2,3,4,5,6,7,8,9] )
 
 
-def not_dubplicated(max_row = 9, max_column = 9):
+def not_duplicated(max_row = 9, max_column = 9):
     clauses = []
-
+    for r in range(1,max_row + 1): 
+        for c in range(1, max_column + 1):
+            for v in range(1, 9 + 1):
+                for v2 in range(v + 1, 9 + 1):
+                    clauses.append([-rcv(r,c,v), -rcv(r,c,v2)])
     return clauses
+
+assert(not_duplicated()[7]==[-1, -9])
+assert(not_duplicated()[8]==[-2, -3])
