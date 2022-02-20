8c220ccf46f16a9ac373d030fc2a0a744ba112c2
AJ <anto296@hotmail.com>
2022-02-20 16:45:09 +0100

    more specific clause names

--- lab2/clauses.py
+++ lab2/clauses.py
@@ -5,7 +5,7 @@ def rcv(row, column, value, max_row = 9, max_column = 9):
 assert(rcv(1,1,9) == 9)
 assert(rcv(1,2,6) == 15)
 
-def all_filled(max_row = 9, max_column = 9):
+def at_least_one_value(max_row = 9, max_column = 9):
     clauses = []
     for r in range(1,max_row+1): 
         for c in range(1, max_column+1):
@@ -14,10 +14,10 @@ def all_filled(max_row = 9, max_column = 9):
             )
 
     return clauses
-assert(all_filled()[0] == [1,2,3,4,5,6,7,8,9] )
+assert(at_least_one_value()[0] == [1,2,3,4,5,6,7,8,9] )
 
 
-def not_duplicated(max_row = 9, max_column = 9):
+def only_one_value(max_row = 9, max_column = 9):
     clauses = []
     for r in range(1,max_row + 1): 
         for c in range(1, max_column + 1):
@@ -26,5 +26,11 @@ def not_duplicated(max_row = 9, max_column = 9):
                     clauses.append([-rcv(r,c,v), -rcv(r,c,v2)])
     return clauses
 
-assert(not_duplicated()[7]==[-1, -9])
-assert(not_duplicated()[8]==[-2, -3])
+assert(only_one_value()[7]==[-1, -9])
+assert(only_one_value()[8]==[-2, -3])
+
+def filled_row(max_row = 9, max_column = 9):
+    clauses = []
+
+    return clauses
+
