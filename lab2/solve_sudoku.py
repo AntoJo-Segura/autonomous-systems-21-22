852ebf8588b80d5772ecf1cfb37c3674f03410d7
AJ <anto296@hotmail.com>
2022-03-06 18:02:22 +0100

    refactor main of solution

--- lab2/solve_sudoku.py
+++ lab2/solve_sudoku.py
@@ -1,8 +1,8 @@
-from sudoku_solver import count_solution
-from sudoku_solver import solve
+from sudoku_solver import count_solution, solve
 
-to_solve = input()
-print( solve(to_solve) + '    This is a solution')
-print('Press y if you want to run count algorithm:')
-start = input()
-if start == 'y': count_solution(to_solve, True)
+if __name__ == "__main__":
+    to_solve = input()
+    print( solve(to_solve) + '    This is a solution')
+    print('Press y if you want to run count algorithm:')
+    start = input()
+    if start == 'y': count_solution(to_solve, True)
