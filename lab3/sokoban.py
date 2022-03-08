8d4e3fbbb05b90192b2ba49df7cd79f5572b509f
AJ <anto296@hotmail.com>
2022-03-08 01:40:53 +0100

    from board to pddl

--- lab3/sokoban.py
+++ lab3/sokoban.py
@@ -60,12 +60,78 @@ class SokobanGame(object):
         """ Whether the given coordinate is a goal location. """
         return (x, y) in self.goals
 
+    def get_pddl(self):
+        pddl  = []
+        name = 'simpleone'
+        pddl += ['(define (problem '+ name +')']
+        pddl += ['  (:domain sokoban)']
+        pddl += ['  (:objects']
+        pddl += ['  teletransport - transport']
+        pddl += ['  dir-right - direction']
+        pddl += ['  dir-left - direction']
+        pddl += ['  dir-up - direction']
+        pddl += ['  dir-down - direction']
+        pddl += ['  player-01 - player']
+
+        for i,coord in enumerate(self.boxes):
+            pddl += ['  box-0'+ str(i+1)+' - box']
+        
+        for coord in self.positions:
+            pddl += [' pos-'+ str(coord[0]) +'-'+ str(coord[1]) +' - position']
+        pddl += ['  )']
+        pddl += ['  (:init ']
+        for coord in self.goals:
+            pddl += ['  (is-goal pos-'+ str(coord[0]) +'-'+ str(coord[1]) +')']
+        for i,coord in enumerate(self.boxes):
+            pddl += ['  (at box-0'+ str(i)+' pos-' + str(coord[0]) +'-'+ str(coord[1]) +')']
+
+        pddl += ['  (at player-01 pos-' + str(self.player[0]) +'-' + str(self.player[1]) +')']
+        pddl += ['  (is-transported teletransport)']
+        for coord in self.gaps or coord in self.boxes: #'or' term asked to Andres
+            pddl += ['  (transport-position pos-'+ str(coord[0]) + '-'+ str(coord[1]) +')']
+
+        for coord in self.positions:
+            if coord not in self.goals:
+                pddl += ['  (non-goal pos-'+ str(coord[0])+ '-'+ str(coord[1])+ ')']
+
+        for coord in self.gaps:
+            pddl += ['  (empty pos-'+ str(coord[0]) + '-'+ str(coord[1]) +')']
+
+        for coord in self.gaps:
+            right_pos = (coord[0],coord[1]+1)
+            if ( right_pos in self.gaps or right_pos in self.boxes):#right move
+                pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0])+'-'+str(coord[1]+1)+' dir-right)']
+            if (coord[0],coord[1]-1) in self.gaps or (coord[0],coord[1]-1) in self.boxes :#left move
+                pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0])+'-'+str(coord[1]-1)+' dir-left)']
+            if (coord[0]+1,coord[1]) in self.gaps or (coord[0]+1,coord[1]) in self.boxes :#up move
+                pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0]+1)+'-'+str(coord[1])+' dir-up)']
+            if (coord[0]-1,coord[1]) in self.gaps or (coord[0]-1,coord[1]) in self.boxes :#down move
+                pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0]-1)+'-'+str(coord[1])+' dir-down)']
+
+        coord = self.player
+        right_pos = (coord[0],coord[1]+1)
+        if ( right_pos in self.gaps or right_pos in self.boxes):#right move
+            pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0])+'-'+str(coord[1]+1)+' dir-right)']
+        if (coord[0],coord[1]-1) in self.gaps or (coord[0],coord[1]-1) in self.boxes :#left move
+            pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0])+'-'+str(coord[1]-1)+' dir-left)']
+        if (coord[0]+1,coord[1]) in self.gaps or (coord[0]+1,coord[1]) in self.boxes :#up move
+            pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0]+1)+'-'+str(coord[1])+' dir-up)']
+        if (coord[0]-1,coord[1]) in self.gaps or (coord[0]-1,coord[1]) in self.boxes :#down move
+            pddl +=['  (direction-move pos-'+str(coord[0])+'-'+str(coord[1])+' pos-'+str(coord[0]-1)+'-'+str(coord[1])+' dir-down)']        
+        pddl +=['  (direction-move pos-2-4 pos-2-5 dir-right)']
+        pddl += ['  )']
+        pddl += ['  (:goal (and ']
+        for i,coord in enumerate(self.boxes):
+            pddl += ['  (at-goal box-0'+str(i+1)+'))']
+        pddl += ['  )']
+        pddl += [')']
+        return '\n'.join(pddl) 
 
 def main(argv):
     args = parse_arguments(argv)
     with open(args.i, 'r') as file:
         board = SokobanGame(file.read().rstrip('\n'))
-
+    print(board.get_pddl())
 
     # TODO - Some of the things that you need to do:
     #  1. (Previously) Have a domain.pddl file somewhere in disk that represents the Sokoban actions and predicates.
